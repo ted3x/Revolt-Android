cbe571306109a9a72cf677aa67d34e19fe87e02a
Tedo Manvelidze <67691418+ted3x@users.noreply.github.com>
2023-09-13 23:30:01 +0400

    Initial commit

---
+++ app/src/main/java/ge/ted3x/revolt/MainActivity.kt
@@ -0,0 +1,58 @@
+package ge.ted3x.revolt
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import com.bumble.appyx.navigation.integration.ActivityIntegrationPoint
+import com.bumble.appyx.navigation.integration.NodeHost
+import com.bumble.appyx.navigation.platform.AndroidLifecycle
+import ge.ted3x.revolt.ui.node.DashboardNode
+import ge.ted3x.revolt.ui.theme.RevoltTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var appyxV2IntegrationPoint: ActivityIntegrationPoint
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appyxV2IntegrationPoint = createIntegrationPoint(savedInstanceState)
+        setContent {
+            RevoltTheme {
+                // A surface container using the 'background' color from the theme
+                NodeHost(
+                    lifecycle = AndroidLifecycle(LocalLifecycleOwner.current.lifecycle),
+                    integrationPoint = appyxV2IntegrationPoint,
+                ) {
+                    DashboardNode(it)
+                }
+            }
+        }
+    }
+
+    private fun createIntegrationPoint(savedInstanceState: Bundle?) =
+        ActivityIntegrationPoint(
+            activity = this,
+            savedInstanceState = savedInstanceState
+        )
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        appyxV2IntegrationPoint.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        appyxV2IntegrationPoint.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        appyxV2IntegrationPoint.onSaveInstanceState(outState)
+    }
+}
